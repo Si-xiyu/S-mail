00b5da3b8555cd3b871155ce8ec6354cb95bb09c
Si-xiyu <junxiangluo0@gmail.com>
2026-07-07 13:56:44 +0800

    修改了关于启动问题的一些相关文件

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,11 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': 'http://localhost:8089'
-    }
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8080',
+          changeOrigin: true
+        }
+      }
   }
 })
